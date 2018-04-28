3375a3e88a335d737d1fd8326d0ab5c7229c1133
Edxael <zerphyr.r@gmail.com>
2018-04-28 09:35:39 -0600

    headers

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-// ===[ Import Dependencies ]=========================================================================
+// ===[ Import Dependencies ]========================================================
 const app = require('express')()
-const graphql = require('graphql')
+const expressGQL = require('express-graphql')
+const mySchema = require('')
+
+
+
+
+// ===[ Routes ]====================================================================
+app.use('/pg', expressGQL({
+    schema: mySchema,
+    graphiql: true
+}))
+
+
+
+
+// ===[ Server Listener ]===========================================================
+app.listen(4000, () => {
+    console.log("Servery Listeing on port 4000 ...")
+})
